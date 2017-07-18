bc487918b51a9b01801fab269af7fc0053b5fb84
YraKot <y.kitsera@gmail.com>
2017-07-18 18:29:01 +0300

    make it better

--- keystone.js
+++ keystone.js
@@ -69,4 +69,21 @@ keystone.set('nav', {
 
 // Start Keystone to connect to your database and initialise the web server
 
+if (keystone.get('env') == 'production'){
+    keystone.set('cloudinary config', process.env.CLOUDINARY_URL);
+    keystone.set('cookie secret', process.env.COOKIE_SECRET);
+    keystone.set('mandrill api key', process.env.MANDRILL_API_KEY);
+}
+
+if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+	console.log('----------------------------------------'
+	+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
+	+ '\n----------------------------------------'
+	+ '\nYou have opted into email sending but have not provided'
+	+ '\nmailgun credentials. Attempts to send will fail.'
+	+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
+	+ '\nset up your mailgun integration');
+}
+
+
 keystone.start();
